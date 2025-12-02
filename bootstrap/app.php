ae712e8ce161308055fb4d6f77ad6ed2a075f538
Lokesh <lenkalokesh98@gmail.com>
2025-12-02 10:22:34 +0530

    configured cors.php to support CORS policy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'manage_users' => \App\Http\Middleware\EnsureUserCanManageOtherUsers::class,
             // 'approve_users_by_ebm' => \App\Http\Middleware\EnsureUserIsExecutiveBodyMember::class,
         ]);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
