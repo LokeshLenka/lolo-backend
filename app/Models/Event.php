0406825d3213b9d03784881aa3cc0a20979e2987
Lokesh <lenkalokesh98@gmail.com>
2026-02-23 23:23:51 +0530

    refactor(event): added SoftDeletes from Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -14,7 +14,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     /** @use HasFactory<\Database\Factories\EventFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'uuid',
